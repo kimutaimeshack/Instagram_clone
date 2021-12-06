544396a8a3c8b057c14be0e04d5737f8b0f60167
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-12-06 16:07:13 +0300

    account creation

--- myphotosproject/urls.py
+++ myphotosproject/urls.py
@@ -1,8 +1,10 @@
 
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('photos.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
